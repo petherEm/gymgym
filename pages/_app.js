9499416d430245c9b63c70f934ee49433d19af49
Piotr Maciejewski <pm@piotrs-macbook-pro.home>
2022-09-25 06:54:32 +0200

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,10 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import "../styles/workoutSlider.css";
+import "swiper/css";
+import "swiper/css/navigation";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
